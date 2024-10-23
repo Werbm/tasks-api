7733639c718ce045cece74a8f26be1b061f8e17f
unknown <lwerbm@gmail.com>
2024-10-23 02:49:13 -0300

    creating authentication routes

---
+++ backend/src/routes/UserRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createUser } from "../controllers/UserController";
+import { login } from "../controllers/AuthController";
+
+const UserRouter = Router()
+
+UserRouter.post('/register', createUser)
+UserRouter.post('/login', login)
+
+
+export default UserRouter;
